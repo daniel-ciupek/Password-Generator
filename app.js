cc3d76be225deaec5165c588d49add23a785febd
daniel-ciupek <danter005@o2.pl>
2025-11-12 20:12:47 +0100

    JS code for generating a password

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ class PasswordGenerator {
 
     this.lowercaseCheckbox = document.querySelector("#lowercase");
 
-    this.numbersCheckbox= document.querySelector("#numbers");
+    this.numbersCheckbox = document.querySelector("#numbers");
 
     this.symbolsCheckbox = document.querySelector("#symbols");
 
@@ -20,8 +20,30 @@ class PasswordGenerator {
   }
 
   init() {
-    document.querySelectorAll(".options input [type='checkbox']");
+    document
+      .querySelectorAll(".options input [type='checkbox']")
+      .forEach((cb) => cb.addEventListener("click", this.updateOptions));
   }
 
+  updateOptions = () => {
+    
+  };
+
+  getRandomUppercase() {
+    return String.fromCharCode(65 + Math.floor(Math.random() * 26));
+  }
+
+  getRandomLowercase() {
+    return String.fromCharCode(97 + Math.floor(Math.random() * 26));
+  }
+
+  getRandomNumber() {
+    return Math.floor(Math.random() * 10);
+  }
+
+  getRandomSymbol() {
+    const symbols = `!@#$%^&*()_+-=[]{}|;:'",.<>/?~|`;
+    return symbols[Math.floor(Math.random() * symbols.length)];
+  }
 }
 const passwordGenerator = new PasswordGenerator();
